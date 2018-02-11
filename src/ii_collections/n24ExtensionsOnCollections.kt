8cb600b65a32e5664fc816a9b15e99b49ee8983e
danielmoi <daniel@ampersandmoi.com>
2018-02-11 12:32:23 +1100

    Finish 2/24 Extensions

--- src/ii_collections/n24ExtensionsOnCollections.kt
+++ src/ii_collections/n24ExtensionsOnCollections.kt
@@ -12,8 +12,46 @@ fun todoTask24(): Nothing = TODO(
 )
 
 fun doSomethingStrangeWithCollection(collection: Collection<String>): Collection<String>? {
-    val groupsByLength = collection.groupBy { s -> todoTask24() }
+    val groupsByLength = collection.groupBy { str -> str.length }
+    println("groupsByLength:$groupsByLength")
 
-    return groupsByLength.values.maxBy { group -> todoTask24() }
+    return groupsByLength.values.maxBy { group -> group.size }
 }
 
+/*
+groupsByLength:{1=[a, c], 2=[bb]}
+groupsByLength:{0=[, , , ], 1=[a], 2=[bb], 3=[ccc], 4=[dddd]}
+groupsByLength:{1=[a, c], 2=[bb, dd]}
+groupsByLength:{}
+groupsByLength:{1=[a]}
+ */
+
+/*
+class _24_JavaCode : JavaCode() {
+    fun doSomethingStrangeWithCollection(collection: Collection<String>): Collection<String>? {
+        val groupsByLength = Maps.newHashMap()
+        for (s in collection) {
+            var strings: MutableList<String>? = groupsByLength.get(s.length)
+            if (strings == null) {
+                strings = Lists.newArrayList()
+                groupsByLength.put(s.length, strings)
+            }
+            strings.add(s)
+        }
+
+        var maximumSizeOfGroup = 0
+        for (group in groupsByLength.values) {
+            if (group.size > maximumSizeOfGroup) {
+                maximumSizeOfGroup = group.size
+            }
+        }
+
+        for (group in groupsByLength.values) {
+            if (group.size == maximumSizeOfGroup) {
+                return group
+            }
+        }
+        return null
+    }
+}
+*/
